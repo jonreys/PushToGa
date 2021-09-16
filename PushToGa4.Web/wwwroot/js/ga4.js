f13dd8d6bb858bd0386faa1ccc76cfa4e8361e32
jonathan.reyes <jonreys@gmail.com>
2021-09-16 10:36:46 +0800

    Complete Commit

---
+++ PushToGa4.Web/wwwroot/js/ga4.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    document.title = document.title.substring(0, document.title.lastIndexOf("/"));
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+
+    gtag('config', 'G-01JM05KNTW');
+
+    $("#btnSend").click(function () {
+        var userId = $("#userId").val();
+        var userName = $("#username").val();
+        var type = $("#optionTypeId option:selected").val();
+
+        if (userId != null && userId.length > 0) {
+            gtag("event", "login", {
+                method: "User ID:" + userId + " - " + type
+            });
+
+            alert('Sent');
+        }
+        else
+            alert('Oooops, User ID is not present.');
+    });
+});
